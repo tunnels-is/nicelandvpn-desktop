15ec81d75e1252f491b7dfdf79add90e6afbc454
Tunnels <tunnels@tunnels-ehf.local>
2023-08-03 19:52:13 +0000

    c code launcher complete, fixes clipboard issues and admin launch issues

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -1,40 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <Security/Authorization.h>
+#import <string.h>
+#import <unistd.h>
+#import <CoreFoundation/CoreFoundation.h>
 
-int main() {
-    // Replace with the path to the program you want to execute with elevated privileges
-    const char *programPath = "./NicelandVPN";
+int main(int argc, char *argv[]) {
 
-    // Create the AuthorizationRef object
-    AuthorizationRef authRef;
+CFURLRef url = CFBundleCopyExecutableURL(CFBundleGetMainBundle());
+char path[PATH_MAX];
+CFURLGetFileSystemRepresentation(url, true, (UInt8*)path, sizeof(path));
+CFRelease(url);
+// printf("DIR:%s\n", cwd_result);
+// // printf("DIR:%s\n", argv[0]);
+// char *dirPath = strtok(path, "/launcher");
+int size = strlen(path);
+path[size-9] = '\0';
+char fullPath[550];
+strcpy(fullPath,path);
+strcat(fullPath, "/NicelandVPN");
+printf("DIR:%s\n", fullPath);
+// // printf("DIR: %s\n", fullPath);
+// printf("FU§LL:%s\n", fullPath );
+
+
+  const char *authRightName = "system.privilege.admin";
+    AuthorizationItem right = {authRightName, 0, NULL, 0};
+    AuthorizationRights rights = {1, &right};
+    AuthorizationFlags flags = kAuthorizationFlagExtendRights | kAuthorizationFlagInteractionAllowed | kAuthorizationFlagPreAuthorize;
+
+    AuthorizationRef authRef = NULL;
     OSStatus status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &authRef);
     if (status != errAuthorizationSuccess) {
         printf("Error creating authorization reference: %d\n", status);
         return 1;
     }
 
-    // Set the right to run the command as the root user
-    AuthorizationItem authItems = {kAuthorizationRightExecute, 0, NULL, 0};
-    AuthorizationRights authRights = {1, &authItems};
-
-    // Acquire the necessary privileges
-    status = AuthorizationCopyRights(authRef, &authRights, kAuthorizationEmptyEnvironment, kAuthorizationFlagPreAuthorize, NULL);
+    status = AuthorizationCopyRights(authRef, &rights, NULL, flags, NULL);
     if (status != errAuthorizationSuccess) {
         printf("Error acquiring rights: %d\n", status);
         AuthorizationFree(authRef, kAuthorizationFlagDestroyRights);
         return 1;
     }
 
-    // Execute the program with administrator privileges
-    FILE *output = NULL;
-    char *arguments[] = {NULL}; // Add program arguments if needed
-    status = AuthorizationExecuteWithPrivileges(authRef, programPath, kAuthorizationFlagDefaults, arguments, &output);
+    char *arguments[] = { NULL}; 
+    status = AuthorizationExecuteWithPrivileges(authRef, fullPath, kAuthorizationFlagDefaults, arguments, NULL);
     if (status != errAuthorizationSuccess) {
         printf("Error executing program: %d\n", status);
     }
+    printf("STATUS %d\n", status);
 
-    // Clean up resources
-    if (output != NULL) fclose(output);
     AuthorizationFree(authRef, kAuthorizationFlagDestroyRights);
 
     return 0;

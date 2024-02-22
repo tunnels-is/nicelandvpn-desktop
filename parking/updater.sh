7b655ff3595ee0e0b966f9528ff06fe849b813d7
Artur Stuhl <150718978+Arthur-Stuhl@users.noreply.github.com>
2024-02-22 02:40:59 +0000

    Update and rename pacman.sh to updater.sh
    Fixed file not downloading and also removed some code and uncommitted a bunch of code from the script as this is only a temporary fix, actually I wouldn't even call it a fix at all. but at least it downloads for now.

--- parking/updater.sh
+++ parking/updater.sh
@@ -21,35 +21,30 @@
 
 #--------------------------------------------------------------------------------
 
-# This is niceland-update v0.1
+# This is niceland-update v0.2
 
 
 # this file must be present to track if the release has changed
-RELEASE_FILE="/home/anon/release"
- 
+RELEASE_FILE="/home/$USER/.config/NicelandVPN/release-version"
+
 # get the release tag from the latest release URL
-RELEASE_TAG=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/tunnels-is/nicelandVPN/releases/latest | awk -F '/' '{print $NF}')
- 
+RELEASE_TAG=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/tunnels-is/nicelandvpn-desktop/releases/latest | awk -F '/' '{print $NF}')
+
 # Check if the release has changed
 if [ -f "$RELEASE_FILE" ] && [ "$(cat "$RELEASE_FILE")" = "$RELEASE_TAG" ]; then
     echo "Release has not changed"
 else
     echo "Release has changed"
- 
+
     # Replace the 'v' prefix in the release tag with a '-' character
-    DOWNLOAD_TAG=$(echo "$RELEASE_TAG" | sed 's/^v/-/')
- 
-    DOWNLOAD_URL="https://github.com/tunnels-is/nicelandVPN/releases/download/$RELEASE_TAG/nicelandVPN$DOWNLOAD_TAG-linux.pacman"
-    wget $DOWNLOAD_URL
- 
-    #echo "nicelandVPN$DOWNLOAD_TAG-linux.pacman"
-    sudo pacman -U "nicelandVPN$DOWNLOAD_TAG-linux.pacman"
- 
+    #DOWNLOAD_TAG=$(echo "$RELEASE_TAG" | sed 's/^v/-/')
+
+    #DOWNLOAD_URL="https://github.com/tunnels-is/niceland-desktop/releases/download/$RELEASE_TAG/NicelandVPN-Linux"
+    #wget $DOWNLOAD_URL
+    wget https://github.com/tunnels-is/nicelandvpn-desktop/releases/download/v1.1.5/NicelandVPN-Linux
+    
     # Update the release file with the new release tag
     echo "$RELEASE_TAG" > "$RELEASE_FILE"
-    
-    # Removes downloaded package after install
-    rm -rf ./nicelandVPN-*-linux.*
 
     # Notifies that the update has been completed
     printf "\nUpdate completed. \nnicelandVPN has been updated to the latest version\n"
